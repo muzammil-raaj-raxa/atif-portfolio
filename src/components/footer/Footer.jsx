6c581d21cf4e0ac98aed8eaa8187593a32e6f211
Muzammil Raza <muzammilraj0001@gmail.com>
2024-01-01 22:48:47 +0500

    .

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,6 +9,10 @@ const Footer = () => {
 
                 <div className="footer__social">
 
+                <a href="https://atifzahoorarsh@gmail.com" className="footer__social-link" target='_blank'>
+                        <i class="bx bxl-gmail"></i>
+                    </a>
+
                      <a href="https://www.linkedin.com/in/atif-zahoor-3026b82a7?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" className="footer__social-link" target='_blank'>
                         <i class="bx bxl-linkedin"></i>
                     </a>
@@ -17,9 +21,7 @@ const Footer = () => {
                         <i class="bx bxl-whatsapp"></i>
                     </a>
 
-                    <a href="https://atifzahoorarsh@gmail.com" className="footer__social-link" target='_blank'>
-                        <i class="bx bxl-gmail"></i>
-                    </a>
+
 
                     <a href="https://atifzahoorarsh@gmail.com" className="footer__social-link" target='_blank'>
                         <i class="bx bxl-medium"></i>
